f4d8517b92e780fe64ca2bb442c34776858e7574
Jyri Nieminen <79329227+LeDevilCat@users.noreply.github.com>
2025-03-02 11:59:30 +0200

    tiedostosta luku

--- ConsoleApplication1/MyGrep.cpp
+++ ConsoleApplication1/MyGrep.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    // Suomenkielinen merkistö
+    // Asetetaan paikallinen asetukset suomen kielelle
     setlocale(LC_ALL, "fi_FI");
 
+    // Jos komentoriviparametreja ei ole annettu
     if (argc == 1) {
         // Käyttäjä syöttää lauseen ja hakusanan
         string sana, lause;
@@ -16,7 +18,7 @@ int main(int argc, char* argv[]) {
         cout << endl << "Syötä hakusana: ";
         getline(cin, sana);
 
-        // Etsitään hakusana lauseesta
+        // Etsitään hakusanaa lauseesta
         size_t index = lause.find(sana);
         if (index != string::npos) {
             cout << "Hakusana löytyi lauseesta kohdasta " << index << endl;
@@ -25,28 +27,57 @@ int main(int argc, char* argv[]) {
             cout << "Hakusanaa ei löytynyt lauseesta" << endl;
         }
     }
+    // Jos komentoriviparametreja on 3 tai 4
+    else if (argc == 3 || argc == 4) {
+        bool l_option = false, o_option = false;
+        string hakusana, tiedostonimi;
 
-    else if (argc == 3) {
-        // VAIHE 2: Luetaan tiedosto ja etsitään hakusanaa**
-        string hakusana = argv[1];  // Hakusana tulee komentoriviltä
-        string tiedostonimi = argv[2];  // Tiedostonimi tulee komentoriviltä
+        // Jos optioita on annettu
+        if (argc == 4) {
+            string optiot = argv[1];
+            if (optiot.find('l') != string::npos) l_option = true;
+            if (optiot.find('o') != string::npos) o_option = true;
+            hakusana = argv[2];
+            tiedostonimi = argv[3];
+        }
+        // Jos optioita ei ole annettu
+        else {
+            hakusana = argv[1];
+            tiedostonimi = argv[2];
+        }
 
-        ifstream tiedosto(tiedostonimi);  // Avataan tiedosto
+        // Avataan tiedosto
+        ifstream tiedosto(tiedostonimi);
         if (!tiedosto) {
             cerr << "Virhe: Tiedostoa ei voitu avata!" << endl;
             return 1;
         }
 
         string rivi;
-        while (getline(tiedosto, rivi)) {  // Luetaan tiedosto rivi kerrallaan
-            if (rivi.find(hakusana) != string::npos) {  // Jos hakusana löytyy riviltä
-                cout << rivi << endl;  // Tulostetaan rivi
+        int rivinumero = 0;
+        int loydetytRivit = 0;
+
+        // Luetaan tiedosto rivi kerrallaan
+        while (getline(tiedosto, rivi)) {
+            rivinumero++;
+
+            // Jos hakusana löytyy riviltä
+            if (rivi.find(hakusana) != string::npos) {
+                if (l_option) cout << rivinumero << ": ";
+                cout << rivi << endl;
+                loydetytRivit++;
             }
         }
 
-        tiedosto.close();  // Suljetaan tiedosto lopuksi
-    }
+        // Jos 'o' optio on annettu, tulostetaan löytyneiden rivien määrä
+        if (o_option) {
+            cout << "Occurrences of lines containing \"" << hakusana << "\": " << loydetytRivit << endl;
+        }
 
+        // Suljetaan tiedosto
+        tiedosto.close();
+    }
+    // Jos komentoriviparametreja on virheellinen määrä
     else {
         cerr << "Virheellinen määrä argumentteja!" << endl;
         return 1;
